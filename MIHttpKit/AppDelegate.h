9ae7f4247b2f09b50127b6635e4721b8f2644478
丰硕 <fengshuo@qianfan365.com>
2016-01-19 17:03:28 +0800

    Initial Commit

---
+++ MIHttpKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MIHttpKit
+//
+//  Created by qf on 16/1/19.
+//  Copyright © 2016年 MI-31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MIHttpKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MIHttpKit
+//
+//  Created by qf on 16/1/19.
+//  Copyright © 2016年 MI-31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
